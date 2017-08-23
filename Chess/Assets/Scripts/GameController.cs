0a1bd71762eace61aeb0099405876eaefcf5cbbe
alhulljar <31191366+alhulljar@users.noreply.github.com>
2017-08-22 23:08:32 -0400

    Set Up Repo w/ gitignore

---
+++ Chess/Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public bool selection = true;
+    public bool newSelect = false;
+    public MoveWhite[] whiteProp = new MoveWhite[16];
+    public MoveBlack[] blackProp = new MoveBlack[16];
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+
+    private GameObject[] white = new GameObject[16];
+    private GameObject[] black = new GameObject[16];
+
+    void Start ()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+        cam3.enabled = false;
+        white = GameObject.FindGameObjectsWithTag("White");
+        black = GameObject.FindGameObjectsWithTag("Black");
+        for (int i = 0; i < white.Length; i++)
+        {
+            whiteProp[i] = white[i].GetComponent<MoveWhite>();
+            blackProp[i] = black[i].GetComponent<MoveBlack>();
+        }
+    }
+	
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+            cam3.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+            cam3.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = true;
+        }
+    }
+
+    public void DeselectAll()
+    {
+        for (int i = 0; i < whiteProp.Length; i++)
+        {
+            whiteProp[i].Deselect();
+            blackProp[i].Deselect();
+        }
+    }
+}
---
+++ Chess/Assets/Scripts/MoveWhite.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWhite : MonoBehaviour {
+
+    public int moveSpeed = 15;
+    public Transform piecePos;
+    public Rigidbody rb;
+    private int jumpStr = 30;
+    public bool control = false;
+    public Renderer rend;
+
+    private GameController gameControl;
+    private float lerpFloat = 0.2f;
+    private Color curColor;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        piecePos = GetComponent<Transform>();
+        rend = GetComponent<Renderer>();
+        gameControl = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        curColor = rend.material.color;
+    }
+
+    void Update()
+    {
+        if (control == true & (gameControl.cam1.enabled == true | gameControl.cam2.enabled == true))
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position = piecePos.position + new Vector3(0, 0, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position = piecePos.position + new Vector3(0, 0, -1);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = piecePos.position + new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = piecePos.position + new Vector3(1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * (jumpStr * 10));
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Deselect();
+            }
+        }
+        else if (control == true & gameControl.cam3.enabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position = piecePos.position + new Vector3(0, 0, -1);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position = piecePos.position + new Vector3(0, 0, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = piecePos.position + new Vector3(1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = piecePos.position + new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * (jumpStr * 10));
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                control = false;
+                gameControl.selection = true;
+                rend.material.color = curColor;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Deselect();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (gameControl.selection == true)
+        {
+            gameControl.newSelect = true;
+            gameControl.selection = false;
+            gameControl.DeselectAll();
+            Select();
+        }
+        else if (gameControl.newSelect == true)
+        {
+            gameControl.newSelect = false;
+            gameControl.selection = true;
+            gameControl.DeselectAll();
+            Select();
+        }
+    }
+
+    public void Deselect()
+    {
+        rend.material.color = curColor;
+        control = false;
+    }
+
+    void Select()
+    {
+        control = true;
+        rend.material.color = Color.LerpUnclamped(rend.material.color, Color.cyan, lerpFloat);
+    }
+}
